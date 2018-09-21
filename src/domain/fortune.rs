15e42b0b0e332e5fde5021722eb733caf1685359
rustydice <fer.ramirez2192@gmail.com>
2018-09-21 11:53:03 -0700

    Added tests directory and set balance method to fortune.rs

--- src/domain/fortune.rs
+++ src/domain/fortune.rs
@@ -1,10 +1,19 @@
 #[derive(Debug)]
 pub struct Fortune {
-	pub balance: f64,
+	balance: f64,
 }
 
 impl Fortune{
+
+	pub fn new() -> Fortune{
+		Fortune {balance: 0.0}
+	}
+
 	pub fn get_balance(self) -> f64{
 		self.balance
+	}
+
+	pub fn set_balance(&mut self, balance: f64) {
+		self.balance = balance;
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ mod domain;
 use domain::Fortune;
 
 fn main(){
-	let fortune = Fortune{
-		balance : 50.32,
-	};
+	let fortune = Fortune::new();
 	let balance = fortune.get_balance();
 	println!("{}",balance);
 }
